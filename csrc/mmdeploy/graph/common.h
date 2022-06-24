0c143a5f0bc9fc701afca5468402b813f30e2572
zhangli <lzhang329@gmail.com>
2022-06-24 23:17:17 +0800

    backward compatibility

--- csrc/mmdeploy/graph/common.h
+++ csrc/mmdeploy/graph/common.h
@@ -24,34 +24,7 @@ inline std::true_type Check(T&&) {
 
 }  // namespace
 
-template <typename EntryType, typename RetType = typename Creator<EntryType>::ReturnType>
-inline Result<RetType> CreateFromRegistry(const Value& config, const char* key = "type") {
-  MMDEPLOY_INFO("config: {}", config);
-  auto type = config[key].get<std::string>();
-  auto creator = Registry<EntryType>::Get().GetCreator(type);
-  if (!creator) {
-    MMDEPLOY_ERROR("failed to find module creator: {}", type);
-    return Status(eEntryNotFound);
-  }
-  auto inst = creator->Create(config);
-  if (!Check(inst)) {
-    MMDEPLOY_ERROR("failed to create module: {}", type);
-    return Status(eFail);
-  }
-  return std::move(inst);
-}
-
-
-
 Result<std::vector<std::string>> ParseStringArray(const Value& value);
-
-template <typename BuilderType>
-inline Result<std::unique_ptr<Node>> BuildFromConfig(Value config) {
-  BuilderType builder(std::move(config));
-  OUTCOME_TRY(builder.SetInputs());
-  OUTCOME_TRY(builder.SetOutputs());
-  return builder.Build();
-}
 
 }  // namespace mmdeploy::graph
 
